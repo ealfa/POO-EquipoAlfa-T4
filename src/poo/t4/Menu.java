767f19db39ac33eacae2825f241f085d4f72c5f0
jesus_ignacio_159 <jesus_ignacio_159@10.34.137.109>
2016-04-23 15:24:21 -0400

    first commit

--- src/poo/t4/Menu.java
+++ src/poo/t4/Menu.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package poo.t4;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -16,12 +17,13 @@ import java.util.logging.Logger;
  * @author osilru
  */
 public class Menu extends JFrame implements ActionListener {
+
     JMenuItem cmdAltaCliente, cmdRegistroCompra, cmdPagos, cmdConsultaSaldo, cmdConfiguracion;
     JMenu menuClientes, menuMovimientos, menuConfiguracion;
     JMenuBar braMenu;
-    
+
     //Constructor
-    public Menu(){
+    public Menu() {
         //Establecemos el título de la ventana.
         super("Bienvenido");
         //Establecemos la ubicación en la pantalla y las dimenciones de la ventana
@@ -29,65 +31,63 @@ public class Menu extends JFrame implements ActionListener {
 //        Cliente cliente = new Cliente();
 //        Bitacora bitacora = new Bitacora();
 //        Transferencia transferencia = new Transferencia();
-        
-        setBounds(200,200,400,400); //x,y,ancho,alto        
+
+        setBounds(200, 200, 400, 400); //x,y,ancho,alto        
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Programamos la X para cerrar la ventana               
         setLayout(new FlowLayout());
         setResizable(false);
-        
+
         //Paso 1. Crear los JMenuItems
         cmdAltaCliente = new JMenuItem("Alta de Cliente");
         cmdRegistroCompra = new JMenuItem("Registro de Compras");
         cmdPagos = new JMenuItem("Pagos");
         cmdConsultaSaldo = new JMenuItem("Consulta de Saldo");
         cmdConfiguracion = new JMenuItem("Porcentaje de Acomulado");
-        
-       
+
         //Paso 2. Creamos los JMenus
         menuClientes = new JMenu("Clientes");
         menuMovimientos = new JMenu("Movimientos");
         menuConfiguracion = new JMenu("Configuración");
-        
+
         //Paso 3. Creamos la Barra JMenuBar
         braMenu = new JMenuBar();
-        
+
         //Paso 4. Agregar los items a los menus
         menuClientes.add(cmdAltaCliente);
         menuMovimientos.add(cmdRegistroCompra);
         menuMovimientos.add(cmdPagos);
         menuMovimientos.add(cmdConsultaSaldo);
         menuConfiguracion.add(cmdConfiguracion);
-        
+
         //Paso 5. Agregar los menus a la barra
         braMenu.add(menuClientes);
         braMenu.add(menuMovimientos);
         braMenu.add(menuConfiguracion);
-        setJMenuBar(braMenu);        
+        setJMenuBar(braMenu);
         setVisible(true);
-        
+
         //Paso 6. Que los comandos ESCUCHEN
         cmdAltaCliente.addActionListener(this);
         cmdRegistroCompra.addActionListener(this);
         cmdPagos.addActionListener(this);
         cmdConsultaSaldo.addActionListener(this);
-        cmdConfiguracion.addActionListener(this);    
+        cmdConfiguracion.addActionListener(this);
     }
-    
+
 //    private void salir(){
 //        System.exit(0);
 //    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == cmdAltaCliente){
-                Ventana1 ventana1 = new Ventana1();
-        } else if (e.getSource() == cmdRegistroCompra){
-                Ventana2 ventana2 = new Ventana2();
-        } else if (e.getSource() == cmdPagos){
-                Ventana3 ventana3 = new Ventana3();
-        } else if (e.getSource() == cmdConsultaSaldo){
+        if (e.getSource() == cmdAltaCliente) {
+            Ventana1 ventana1 = new Ventana1();
+        } else if (e.getSource() == cmdRegistroCompra) {
+            Ventana2 ventana2 = new Ventana2();
+        } else if (e.getSource() == cmdPagos) {
+            Ventana3 ventana3 = new Ventana3();
+        } else if (e.getSource() == cmdConsultaSaldo) {
             Ventana4 ventana4 = new Ventana4();
-        } else if (e.getSource() == cmdConfiguracion){
+        } else if (e.getSource() == cmdConfiguracion) {
             Ventana5 ventana5 = new Ventana5();
         }
     }
